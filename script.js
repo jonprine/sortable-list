6240a642b3c1ae9dbd891885a276ec0ebd9aef6c
Vox Humana <jonprine@gmail.com>
2021-01-09 07:39:39 -0600

    added list to ui

---
+++ script.js
@@ -0,0 +1,45 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const favFilmakers = [
+    'Stanley Kubrick',
+    'Francis Ford Coppola',
+    'Quentin Tarantino',
+    'David Lynch',
+    'Martin Scorcese',
+    'John Carpenter',
+    'Spike Lee',
+    'Richard Linklater',
+    'PT Anderson',
+    'David Cronenberg'
+];
+
+// store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// insert list items into dom
+function createList() {
+    [...favFilmakers]
+        .forEach((person, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+            <p class="person-name">${person}</p>
+            <i class="fas fa-grip-lines"</i>
+            </div>
+            `;
+
+            listItems.push(listItem);
+
+            draggable_list.appendChild(listItem);
+
+        });
+}
